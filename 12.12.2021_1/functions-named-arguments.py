4a23a3fdc03c967925d253ad655c7b3e97cb189c
Marcin Krawczyk <marckraw@icloud.com>
2021-12-12 15:56:10 +0100

    [12.12.2021] - args kwargs

--- 12.12.2021_1/functions-named-arguments.py
+++ 12.12.2021_1/functions-named-arguments.py
@@ -8,3 +8,12 @@ fun(a=1, b=2, c=3)
 fun(a=1, c=3, b=2)
 
 fun(1, b=2)
+
+
+
+###########################################3
+def fun2(a, b, c, *, d, e): # w taki sposob mozemy zmusic uzytkownika zeby d i e przekazal jako named
+    print(a, b, c, d, e)
+
+
+fun2(1, 2, 3, d=4, e=5)
